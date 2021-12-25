747acfcf76dbb7c3f850e9ac54f5c4cdb6096d44
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2021-12-25 21:30:41 +0900

    add models

--- src/models/model_make.py
+++ src/models/model_make.py
@@ -3,6 +3,7 @@ from keras.layers import Dense, Dropout, Flatten, Activation
 from keras.layers import Conv2D, MaxPooling2D
 from keras.models import Sequential, load_model
 from keras.utils.np_utils import to_categorical
+
 # from keras.utils.vis_utils import plot_model
 import numpy as np
 import matplotlib.pyplot as plt
@@ -25,40 +26,43 @@ y_test = to_categorical(y_test)
 # モデルの定義
 model = Sequential()
 model.add(Conv2D(filters=32, kernel_size=(3, 3), input_shape=(28, 28, 1)))
-model.add(Activation('relu'))
+model.add(Activation("relu"))
 model.add(Conv2D(filters=64, kernel_size=(3, 3)))
-model.add(Activation('relu'))
+model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 model.add(Flatten())
 model.add(Dense(128))
-model.add(Activation('relu'))
+model.add(Activation("relu"))
 model.add(Dropout(0.5))
 model.add(Dense(10))
-model.add(Activation('softmax'))
+model.add(Activation("softmax"))
 
 
-model.compile(loss='categorical_crossentropy',
-              optimizer='adadelta',
-              metrics=['accuracy'])
+model.compile(
+    loss="categorical_crossentropy", optimizer="adadelta", metrics=["accuracy"]
+)
 
-model.fit(X_train, y_train,
-          batch_size=128,
-          epochs=50,
-          verbose=1,
-          validation_data=(X_test, y_test))
+model.fit(
+    X_train,
+    y_train,
+    batch_size=128,
+    epochs=50,
+    verbose=1,
+    validation_data=(X_test, y_test),
+)
 
 # 精度の評価
 scores = model.evaluate(X_test, y_test, verbose=1)
-print('Test loss:', scores[0])
-print('Test accuracy:', scores[1])
+print("Test loss:", scores[0])
+print("Test accuracy:", scores[1])
 
 # データの可視化（検証データの先頭の10枚）
-for i in range(10):
-    plt.subplot(2, 5, i+1)
-    plt.imshow(X_test[i].reshape((28, 28)), 'gray')
-plt.suptitle("10 images of test data", fontsize=20)
-plt.show()
+# for i in range(10):
+#     plt.subplot(2, 5, i + 1)
+#     plt.imshow(X_test[i].reshape((28, 28)), "gray")
+# plt.suptitle("10 images of test data", fontsize=20)
+# plt.show()
 
 # 予測（検証データの先頭の10枚）
 prediction = np.argmax(model.predict(X_test[0:10]), axis=1)
@@ -72,5 +76,5 @@ model.summary()
 #     os.mkdir(result_dir)
 # 重みを保存
 # model.save(os.path.join(result_dir, 'model.h5'))
-model.save('model.h5')
+model.save("./model.h5")
 # files.download('/content/results/model.h5')
