134e0cb61852e92904e6fe2f063a779ab64a4f94
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2021-12-27 17:57:33 +0900

    added frontend resources

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, redirect, render_template, flash
+from flask import Flask, request, redirect, render_template, flash, jsonify
 from werkzeug.utils import secure_filename
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
@@ -9,7 +9,7 @@ import numpy as np
 classes = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 image_size = 28
 UPLOAD_FOLDER = "uploads"
-ALLOWED_EXTENSIONS = set(["png", "jpg", "jpeg", "gif"])
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
 # 学習済みモデルをロード
 model = load_model("./models/model.h5")
 # app = Flask(__name__, static_folder="../ui/dist/static", template_folder="../ui/dist")
@@ -20,8 +20,29 @@ def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+class InvalidAPIUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        super().__init__()
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv["message"] = self.message
+        return rv
+
+
+@app.errorhandler(InvalidAPIUsage)
+def invalid_api_usage(e):
+    return jsonify(e.to_dict())
+
+
 @app.route("/", methods=["GET", "POST"])
-def upload_file():
+def index():
     if request.method == "POST":
         if "file" not in request.files:
             flash("ファイルがありません")
@@ -37,11 +58,11 @@ def upload_file():
 
             # 受け取った画像を読み込み、np形式に変換
             img = image.load_img(
-                filepath, grayscale=True, target_size=(image_size, image_size)
+                filepath, color_mode="grayscale", target_size=(image_size, image_size)
             )
             img = image.img_to_array(img)
             data = np.array([img])
-            # 変換したデータをモデルに渡して予測する
+            # 変換したデータをモデルに渡して予測
             result = model.predict(data)[0]
             predicted = result.argmax()
             predict_answer = "これは " + classes[predicted] + " です"
@@ -50,6 +71,30 @@ def upload_file():
     return render_template("index.html", answer="")
 
 
+@app.route("/upload", methods=["POST"])
+def upload_file():
+    if "file" not in request.files:
+        raise InvalidAPIUsage("file not found")
+    file = request.files["file"]
+    if file.filename == "":
+        raise InvalidAPIUsage("file not found")
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(UPLOAD_FOLDER, filename))
+        filepath = os.path.join(UPLOAD_FOLDER, filename)
+        img = image.load_img(
+            filepath, color_mode="grayscale", target_size=(image_size, image_size)
+        )
+        img = image.img_to_array(img)
+        data = np.array([img])
+        # 変換したデータをモデルに渡して予測
+        result = model.predict(data)[0]
+        predicted = result.argmax()
+        predict_answer = "これは " + classes[predicted] + " です"
+        print(predict_answer)
+        return jsonify({"answer": predict_answer})
+
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
---
+++ ui/src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+<v-app-bar
+  app
+  color="primary"
+  dark
+>
+  <div class="d-flex align-center">
+    <v-img
+      alt="Vuetify Logo"
+      class="shrink mr-2"
+      contain
+      src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+      transition="scale-transition"
+      width="40"
+    />
+  </div>
+
+  <v-spacer></v-spacer>
+
+  <v-btn icon>
+    <router-link to="/"><v-icon color="white">mdi-home</v-icon></router-link>
+  </v-btn>
+  <v-btn icon>
+    <router-link to="/about"><v-icon color="white">mdi-read</v-icon></router-link>
+  </v-btn>
+  <v-btn icon>
+    <router-link to="/ai"><v-icon color="white">mdi-human-greeting</v-icon></router-link>
+  </v-btn>
+</v-app-bar>
+</template>
