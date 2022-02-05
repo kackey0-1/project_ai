e64eeda88290c947cc2dafb5c29f7b656bbae843
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2022-02-05 18:14:08 +0900

    fix redirection.

--- src/ext/webui/views.py
+++ src/ext/webui/views.py
@@ -1,5 +1,9 @@
-from flask import abort, render_template
+from flask import render_template, redirect, url_for
 
 
 def index():
     return render_template("index.html")
+
+
+def redirect_to_index(text):
+    return redirect("/")
