eb3b96fbbe4c6e3914c0027cd4f6ce13ad96b15f
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2022-02-10 01:11:17 +0900

    update celery.

--- aws-resource/initializer.sh
+++ aws-resource/initializer.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
-echo "hello world!"
 git clone https://github.com/kackey0-1/project_ai.git
 cd project_ai
 python3 --version
 
 sudo apt update
-sudo apt install -y python3-pip python3.8-venv python-celery-common
+sudo apt install -y python3-pip python3-venv python-celery-common python3-h5py
+sudo apt install -y docker docker-compose
+sudo usermod -aG docker ${USER}
+sudo su ${USER}
 python3 -m venv .venv
 source .venv/bin/activate
-pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
+sudo su
+source .venv/bin/activate
+flask run --host 0.0.0.0 --port 80
