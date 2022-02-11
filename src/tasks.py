b918b9f46ec88bc74d819b6469b5ac91302c98d1
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2022-02-11 13:19:43 +0900

    update celery job

--- src/tasks.py
+++ src/tasks.py
@@ -1,8 +1,12 @@
 from src import db
 from .models import Sentence, Status
 from src.ext import celeryapp
+from transformers import T5Tokenizer, AutoModelForCausalLM
 
 celery = celeryapp.celery
+# set tokenizer and model
+tokenizer = T5Tokenizer.from_pretrained("./japanese-gpt2-medium")
+model = AutoModelForCausalLM.from_pretrained("./japanese-gpt2-medium")
 
 
 @celery.task()
@@ -12,12 +16,11 @@ def handle_message(input_uuid):
     sentence = Sentence.query.filter_by(id=input_uuid).first()
     sentence.status = Status.FINISHED
     sentence.output = "FINISHED"
+    input_data = tokenizer.encode(sentence.input, return_tensors="pt")
+    output = model.generate(
+        input_data, do_sample=True, max_length=100, num_return_sequences=8
+    )
+    sentence.output = output
+    print(tokenizer.batch_decode(output))
     db.session.add(sentence)
     db.session.commit()
-    # handle_message.delay(input_uuid)
-    # input_data = tokenizer.encode(sentence.input, return_tensors="pt")
-    # output = model.generate(
-    #     input_data, do_sample=True, max_length=100, num_return_sequences=8
-    # )
-    # sentence.output = input_uuid
-    # print(tokenizer.batch_decode(output))
