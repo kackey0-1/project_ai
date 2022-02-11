d7182c054971f124cee994d5969af90b29ac5caa
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2022-02-11 16:15:34 +0900

    fix initializer.sh

--- aws-resource/initializer.sh
+++ aws-resource/initializer.sh
@@ -7,12 +7,12 @@ python3 --version
 sudo apt update
 sudo apt install -y python3-pip python3-venv python-celery-common python3-h5py
 sudo apt install -y docker docker-compose
-sudo usermod -aG docker ${USER}
-sudo su ${USER}
+#sudo usermod -aG docker ${USER}
+#sudo su ${USER}
 python3 -m venv .venv
 source .venv/bin/activate
 python -m pip install -r requirements.txt
 
-sudo su
-source .venv/bin/activate
-flask run --host 0.0.0.0 --port 80
+#sudo su
+#source .venv/bin/activate
+#flask run --host 0.0.0.0 --port 80
